e6e08c1b21117316e7bc124c82d772ec9a8387fd
karan bamal <51521112+abankalarm@users.noreply.github.com>
2021-11-30 23:53:15 +0530

    spellbee

--- website/views.py
+++ website/views.py
@@ -13,12 +13,25 @@ correct_spellings = words.words()
 def index():
     return render_template("index.html", error = False)
 
-@views.route("/finderrors", Method=['POST'])
+@views.route("/finderrors", Method=['GET','POST'])
 def home():
     textfile = request.args['original']
-    inputwords = split(textfile)
+    inputwords = split(textfile," ")
+    dict = {}
     for i in inputwords:
         if i not in correct_spellings:
-            print("x")
+            if(i.__contains__('_')):
+                entry = i.split('_')[0]
+                dict[i] = functions.autocomplete(entry)
+            else:
+                spellbee = []
+                edit = functions.edit_distance(i)
+                trigram = functions.Jaccard_trigram(i)
+                fourgram = functions.Jaccard_fourgram(i)
+                spellbee.append(edit)
+                spellbee.append(trigram)
+                spellbee.append(fourgram)
+                dict[i] = spellbee
+
     return render_template("index.html")
  
